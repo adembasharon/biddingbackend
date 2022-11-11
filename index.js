53e9054a291309248443ac1022368f1d8298e634
adembasharon <adembasharon816@gmail.com>
2022-11-11 12:39:24 +0300

    modified search

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express=require("express")
 const authRoutes=require("./routes/auth")
 const postRoutes=require("./routes/post")
 const mpesaRoutes=require("./routes/mpesaRoutes")
+const searchRoutes=require("./routes/search")
 const app=express()
 
 const PORT=process.env.PORT || 5000
@@ -26,6 +27,7 @@ app.use(cors())
 app.use("/api",mpesaRoutes)
 app.use("/api/auth",authRoutes)
 app.use("/api/post",postRoutes)
+app.use("/api",searchRoutes)
 app.listen(PORT,()=>{
 console.log(`server running at ${PORT}`)
 })
--- routes/search.js
+++ routes/search.js
@@ -1,8 +1,62 @@
-const algoliasearch = require('algoliasearch')
-const client = algoliasearch('2H16J7JW3T', 'e29fec5ecc8b1323e4a1338d07a8c84c')
-const index = client.initIndex('dev_bid')
-const record = { objectID: 1, name: 'test_record' }
-index.saveObject(record).wait()
-index
-  .search('test_record')
-  .then(({ hits }) => console.log(hits[0]))
+const router=require("express").Router();
+
+  
+  // Instantiate an Algolia client
+const algoliasearch = require('algoliasearch');
+const algoliaClient = algoliasearch('2H16J7JW3T', '663405f6df386091a9c13ddfae6c213e');
+
+// Add the search endpoint
+router.post('/search', async ({body}, res) => {
+  const { requests } = body;
+  const results = await algoliaClient.search(requests);
+  res.status(200).send(results);
+});
+
+// search for facts and values
+router.post('/sffv', async ({body}, res) => {
+  const { requests } = body;
+  const results = await algoliaClient.searchForFacetValues(requests);
+  res.status(200).send(results);
+});
+
+module.exports=router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const algoliasearch = require('algoliasearch')
+// const client = algoliasearch('2H16J7JW3T', 'e29fec5ecc8b1323e4a1338d07a8c84c')
+// const index = client.initIndex('dev_bid')
+// const record = { objectID: 1, name: 'test_record' }
+// index.saveObject(record).wait()
+// index
+//   .search('test_record')
+//   .then(({ hits }) => console.log(hits[0]))
