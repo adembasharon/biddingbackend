31445cfe56a9ae3b577aabfee95ed032e70d3261
adembasharon <adembasharon816@gmail.com>
2022-11-19 18:02:39 +0300

    added cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT=process.env.PORT || 5000
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
-    console.log("Database is Working")
+    console.log("Sharon Database is Working")
 })
 .catch((err)=>{
 console.log(err)
@@ -28,7 +28,7 @@ const corsOptions = {
 
 
 app.use(express.json());
-app.use(cors())
+app.use(cors(corsOptions))
 app.use("/api",mpesaRoutes)
 app.use("/api/auth",authRoutes)
 app.use("/api/post",postRoutes)
