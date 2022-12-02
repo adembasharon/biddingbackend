84d2ceecd2465d1c0d57635dd20968346d0d2c7b
adembasharon <adembasharon816@gmail.com>
2022-12-02 00:24:14 -0800

    removed search routes

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const express=require("express")
 const authRoutes=require("./routes/auth")
 const postRoutes=require("./routes/post")
 const mpesaRoutes=require("./routes/mpesaRoutes")
-const searchRoutes=require("./routes/search")
-// const bidRoutes=require("./routes/bid")
+
 const app=express()
 
 const PORT=process.env.PORT || 5000
@@ -35,8 +34,7 @@ app.use(cors(corsOptions))
 app.use("/api",mpesaRoutes)
 app.use("/api/auth",authRoutes)
 app.use("/api/post",postRoutes)
-app.use("/api",searchRoutes)
-// app.use("/api",bidRoutes)
+
 app.listen(PORT,()=>{
 console.log(`server running at ${PORT}`)
 })
